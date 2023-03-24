822198996d117e5b4e8b2b8b3b217e9bb96d8492
danxdw <daniel_amorim@outlook.pt>
2023-03-24 07:11:51 +0100

    fix

--- editool-7.16/editool/Main.vb
+++ editool-7.16/editool/Main.vb
@@ -487,7 +487,5 @@ Public Class Main
         Console.Write(sender)
     End Sub
 
-    Private Sub ToolStrip1_ItemClicked(sender As Object, e As ToolStripItemClickedEventArgs) Handles ToolStrip1.ItemClicked
 
-    End Sub
 End Class
